a02e2fb6d05205e7c6272646e1924e93bb10e31d
Amin <moradi.amin74@gmail.com>
2020-12-21 09:56:24 +0100

    fixes

--- vampify/src/helpers/baseUrl.ts
+++ vampify/src/helpers/baseUrl.ts
@@ -1,7 +1,3 @@
-export const getApolloClientUrl = (): string | undefined => {
-  return `https://${process.env.REACT_APP_BASE_URL}api${process.env.REACT_APP_STAGE}.rectified.ai/graphql`;
-};
-
 export const getAxiosUrl = (): string | undefined => {
-  return `http://localhost:5000`;
+  return `http://localtoast:5000`;
 };
